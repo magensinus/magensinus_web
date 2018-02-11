489d44a3605b7705ccb5381fc50ef022757770bf
Tony de Faria <wearesocial@iigethr.com>
2018-02-11 22:22:33 +0000

    a bit of cleaning

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # Magensinus settings
+  def magensinus_settings
+    @magensinus_settings = Settings::first!
+  end
+
   # Endorsement badges
   def endorsement_badges
     @endorsement_badges = Endorsement::Badge.where(magensinus: true)
