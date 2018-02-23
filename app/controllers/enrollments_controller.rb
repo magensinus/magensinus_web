278c9fa7b23002bd98cc6e6f500d6a3f942ab002
Tony de Faria <wearesocial@iigethr.com>
2018-02-23 15:27:09 +0000

    housekeeping

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,20 +3,20 @@
 class EnrollmentsController < ApplicationController
   # Callbacks
   # ---------
+  # Academy courses
+  before_action :academy_courses
   # Academy category
   before_action :academy_category
 
   # Index
   # -----
   def index
-    @academy_courses = @academy_category.courses.order(position: :asc)
     @academy_enrollment = @academy_category.enrollments.new
   end
 
   # Create
   # ------
   def create
-    @academy_courses = @academy_category.courses.order(position: :asc)
     @academy_enrollment = @academy_category.enrollments.new(academy_enrollment_params)
     if @academy_enrollment.save
       send_enrollment_email
@@ -35,33 +35,40 @@ class EnrollmentsController < ApplicationController
     @academy_category = Academy::Category.find_by(slug: params[:category_id])
   end
 
+  # Academy courses
+  def academy_courses
+    @academy_courses = @academy_category.courses.order(position: :asc)
+  end
+
   # Send enrollment email
   def send_enrollment_email
+    @academy_enrollment = object
     @courses = []
-    @academy_enrollment_courses = Academy::EnrollmentCourse.where(academy_enrollment_id: @academy_enrollment.id).includes(:course)
+    @academy_enrollment_courses = Academy::EnrollmentCourse.where(academy_enrollment_id: object.id).includes(:course)
     @academy_enrollment_courses.each do |object|
       @courses << object.course.as_json(only: [:title])
     end
+
     client = Postmark::ApiClient.new(ENV["POSTMARK_TOKEN"])
     client.deliver_with_template(
       from: ENV["POSTMARK_NOTIFICATIONS_EMAIL"],
       to: @academy_category.email,
       template_id: "4858883",
       template_model: {
-        name:               @academy_enrollment.name,
-        surname:            @academy_enrollment.surname,
-        dob:                @academy_enrollment.dob,
-        email:              @academy_enrollment.email,
-        phone:              @academy_enrollment.phone,
-        address_one:        @academy_enrollment.address_one,
-        address_two:        @academy_enrollment.address_two,
-        postcode:           @academy_enrollment.postcode,
-        city:               @academy_enrollment.city,
-        country:            @academy_enrollment.country,
-        secondary_name:     @academy_enrollment.secondary_name,
-        secondary_surname:  @academy_enrollment.secondary_surname,
-        secondary_email:    @academy_enrollment.secondary_email,
-        secondary_phone:    @academy_enrollment.secondary_phone,
+        name:               object.name,
+        surname:            object.surname,
+        dob:                object.dob,
+        email:              object.email,
+        phone:              object.phone,
+        address_one:        object.address_one,
+        address_two:        object.address_two,
+        postcode:           object.postcode,
+        city:               object.city,
+        country:            object.country,
+        secondary_name:     object.secondary_name,
+        secondary_surname:  object.secondary_surname,
+        secondary_email:    object.secondary_email,
+        secondary_phone:    object.secondary_phone,
         courses:            @courses
       },
       tag: "enrollment"
