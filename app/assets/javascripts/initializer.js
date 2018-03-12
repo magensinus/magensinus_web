afc88c429211e5eff5cc108f28c563e42b105370
Tony de Faria <wearesocial@iigethr.com>
2018-03-12 17:51:32 +0000

    added a js wrapper

--- app/assets/javascripts/initializer.js
+++ app/assets/javascripts/initializer.js
@@ -2,10 +2,8 @@
 
 bindEvents = function() {
   $(".off").altaiOff();
-
   $(".panel-one").setPanel({ trim: 210 });
   $(".panel-two").setPanel({ trim: 138 });
-
   $(".menu-dropdown").altaiToggle({
     effect: "fade",
     klass: ".menu-dropdown-trigger"
@@ -16,13 +14,29 @@ bindEvents = function() {
   });
 };
 
+bindWrappers = function() {
+  var $lines, holder;
+  $lines = $(".w-col");
+  holder = [];
+  $lines.each(function(i, item) {
+    holder.push(item);
+    if (holder.length === 2) {
+      $(holder).wrapAll('<div class="w-row" />');
+      holder.length = 0;
+    }
+  });
+  $(holder).wrapAll('<div class="w-row" />');
+};
+
 $(document).on( "turbolinks:load", function() {
   bindEvents();
+  bindWrappers();
   Turbolinks.clearCache();
 });
 
 $(window).resize(function() {
   bindEvents();
+  bindWrappers();
   Turbolinks.clearCache();
 });
 
